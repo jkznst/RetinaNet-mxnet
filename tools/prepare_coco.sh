920996172c5b9d9c53d626d754d3d55d206caa0a
zhangxin <2423541783@qq.com>
2018-07-12 17:15:57 +0800

    fix lr_refactor_step bug

--- tools/prepare_coco.sh
+++ tools/prepare_coco.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir ../data/COCO2017rec
-python3 $DIR/prepare_dataset.py --dataset coco2017 --set train2017 --target $DIR/../data/COCO2017rec/train.lst  --root $DIR/../data/COCO2017
-python3 $DIR/prepare_dataset.py --dataset coco2017 --set val2017 --target $DIR/../data/COCO2017rec/val.lst --shuffle False --root $DIR/../data/COCO2017
+mkdir ../data/COCO2017packed
+python3 $DIR/prepare_dataset.py --dataset coco2017 --set train2017 --target $DIR/../data/COCO2017packed/train.lst  --root $DIR/../data/COCO2017
+python3 $DIR/prepare_dataset.py --dataset coco2017 --set val2017 --target $DIR/../data/COCO2017packed/val.lst --shuffle False --root $DIR/../data/COCO2017
