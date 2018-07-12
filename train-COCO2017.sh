f1fede664b3fbfc4ee7d3fba441b16bc382281cd
zhangxin <2423541783@qq.com>
2018-07-12 17:19:30 +0800

    keep pace with prepare_coco.sh

--- train-COCO2017.sh
+++ train-COCO2017.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 train.py --train-path ./data/COCO2017/train.rec --val-path ./data/COCO2017/val.rec --network resnet50 --batch-size 16 --pretrained ./model/resnet-50 --epoch 0 --prefix ./output/exp-COCO-OHEM1/retina --gpus 0,1 --end-epoch 15 --data-shape 512 --label-width 600 --optimizer sgd --lr 0.01 --momentum 0.9 --wd 0.0001 --lr-steps '10, 13' --num-class 80 --class-names ./dataset/names/mscoco.names
+python3 train.py --train-path ./data/COCO2017packed/train.rec --val-path ./data/COCO2017packed/val.rec --network resnet50 --batch-size 16 --pretrained ./model/resnet-50 --epoch 0 --prefix ./output/exp-COCO-FL1/retina --gpus 2,3 --end-epoch 15 --data-shape 512 --label-width 600 --optimizer sgd --lr 0.01 --momentum 0.9 --wd 0.0001 --lr-steps '10, 13' --num-class 80 --class-names ./dataset/names/mscoco.names
