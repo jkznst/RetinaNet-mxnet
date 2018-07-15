d3223266441352e93328eac6e62def8f3d232a48
zhangxin <2423541783@qq.com>
2018-07-15 12:05:17 +0800

    update prepare_pascal.sh

--- tools/prepare_pascal.sh
+++ tools/prepare_pascal.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#python $DIR/prepare_dataset.py --dataset pascal --year 2012 --set train --#target $DIR/../data/train.lst
-#python $DIR/prepare_dataset.py --dataset pascal --year 2012 --set val --#target $DIR/../data/val.lst --shuffle False
 
-python $DIR/prepare_dataset.py --dataset pascal --year 2007,2012 --set trainval --target $DIR/../data/train.lst
-python $DIR/prepare_dataset.py --dataset pascal --year 2007 --set test --target $DIR/../data/val.lst --shuffle False
+python3 $DIR/prepare_dataset.py --dataset pascal --year 2007,2012 --set trainval --target $DIR/../data/VOC2014packed/train.lst
+python3 $DIR/prepare_dataset.py --dataset pascal --year 2007 --set test --target $DIR/../data/VOC2014packed/val.lst --shuffle False
